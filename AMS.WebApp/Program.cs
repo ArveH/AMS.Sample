8dbc6e9476d0fbb9b54f0c24ab87bd4c82cff55c
Arve Hansen <arveh4@outlook.com>
2022-11-20 11:23:13 +0100

    AddAuthentication

--- AMS.WebApp/Program.cs
+++ AMS.WebApp/Program.cs
@@ -1,21 +1,43 @@
+using System.IdentityModel.Tokens.Jwt;
+
 namespace AMS.WebApp;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
-        // Add services to the container.
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = builder.Configuration.GetValue<string>("Auth:Authority");
+
+                options.ClientId = builder.Configuration.GetValue<string>("Auth:ClientId");
+                options.ClientSecret = builder.Configuration.GetValue<string>("Auth:ClientSecret");
+                options.ResponseType = "code";
+
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("email");
+                options.Scope.Add("u4am-public-api");
+
+                options.SaveTokens = true;
+            }); 
         builder.Services.AddRazorPages();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
@@ -24,6 +46,7 @@ public class Program
 
         app.UseRouting();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.MapRazorPages();
