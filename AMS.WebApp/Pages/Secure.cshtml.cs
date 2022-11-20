e04f98998601d6bb70565256143bdcc04bdd5a05
Arve Hansen <arveh4@outlook.com>
2022-11-20 15:00:21 +0100

    Fixed Logout

--- AMS.WebApp/Pages/Secure.cshtml.cs
+++ AMS.WebApp/Pages/Secure.cshtml.cs
@@ -1,16 +1,19 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AMS.WebApp.Pages
-{
-    public class SecureModel : PageModel
-    {
-        public string? UserName { get; set; }
+namespace AMS.WebApp.Pages;
 
-        public void OnGet()
-        {
-            UserName = User.Claims.FirstOrDefault(c => c.Type == "unit4_id")?.Value ?? "<null> (can't find unit4_id claim)";
-        }
+public class SecureModel : PageModel
+{
+    public string? UserName { get; set; }
+
+    public void OnGet()
+    {
+        UserName = User.Claims.FirstOrDefault(c => c.Type == "unit4_id")?.Value ?? "<null> (can't find unit4_id claim)";
+    }
+
+    public IActionResult OnGetLogout()
+    {
+        return SignOut("Cookies", "oidc");
     }
 }
